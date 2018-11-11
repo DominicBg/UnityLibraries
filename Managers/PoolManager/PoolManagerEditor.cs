a8d325d2323b266cc30ef8914e7904dfc1c0c5e8
DominicBg <DominicBg@users.noreply.github.com>
2018-11-11 17:25:12 -0500

    added editor for pool manager

---
+++ Managers/PoolManager/PoolManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+//MUST BE IN EDITOR FOLDER TO COMPILE
+
+[CustomEditor(typeof(PoolManager))]
+public class PoolManagerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PoolManager poolManager = (PoolManager)target;
+        if (GUILayout.Button("Generate Const File"))
+        {
+            poolManager.GenerateConstFile();
+        }
+    }
+}
